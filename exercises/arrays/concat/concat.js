cc50b94afe85d36bdf66882fbaf3cccf54c8ab91
Jesse Farmer <jesse@20bits.com>
2019-11-21 12:56:03 -0800

    Array concat exercise

---
+++ exercises/arrays/concat/concat.js
@@ -0,0 +1,35 @@
+/**
+ * Given two arrays, returns a new array that is a concatenation of the
+ * two given arrays.
+ *
+ * "Concatenation" means the two arrays are joined or glued together, end-to-end.
+ *
+ * JavaScript array's have a built-in concatenation method. Don't use it!
+ * The goal is to better understand how concatenation works, not navigate
+ * the JavaScript documentation.
+ *
+ * See https://en.wikipedia.org/wiki/Concatenation
+ *
+ * @example
+ * concat([1, 2, 3], [4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
+ * concat([-10, undefined], [true, 'waffles']); // => [-10, undefined, true, 'waffles']
+ * concat([], []); // => []
+ * concat([20, 104], []); // => [20, 104]
+ * concat([], ['hello', 'world']); // => ['hello', 'world']
+ *
+ * @param {object[]} leftArray - The left array to concatenate
+ * @param {object[]} rightArray - The right array to concatenate
+ * @returns {object[]} The concatenation of the two given arrays
+ */
+function concat(array) {
+  // This is your job. :)
+}
+
+if (require.main === module) {
+  console.log('Running sanity checks for concat:');
+
+  // Add your own sanity checks here.
+  // How else will you be sure your code does what you think it does?
+}
+
+module.exports = concat;
