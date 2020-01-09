ccb6d49a1b2dbbbfa64380b6cc710e3488d18eed
Jesse Farmer <jesse@20bits.com>
2020-01-08 18:00:09 -0800

    It's spelled 'celsius', not 'celcius'

---
+++ exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
@@ -0,0 +1,17 @@
+/**
+ * Given a temperature in Celsius, return the temperature in Fahrenheit.
+ *
+ * @param {number} temp - The temperature in Celsius
+ * @returns {boolean} The temperature in Fahrenheit
+ */
+function celsiusToFahrenheit(temp) {
+  // This is your job. :).
+}
+
+if (require.main === module) {
+  console.log('Running sanity checks for celsiusToFahrenheit:');
+
+  // Your sanity checks go here.
+}
+
+module.exports = celsiusToFahrenheit;
---
+++ exercises/numbers/fahrenheitToCelsius/fahrenheitToCelsius.js
@@ -0,0 +1,17 @@
+/**
+ * Given a temperature in Fahrenheit, return the temperature in celsius.
+ *
+ * @param {number} temp - The temperature in Fahrenheit
+ * @returns {boolean} The temperature in celsius
+ */
+function fahrenheitToCelsius(temp) {
+  // This is your job. :).
+}
+
+if (require.main === module) {
+  console.log('Running sanity checks for fahrenheitToCelsius:');
+
+  // Your sanity checks go here.
+}
+
+module.exports = fahrenheitToCelsius;
