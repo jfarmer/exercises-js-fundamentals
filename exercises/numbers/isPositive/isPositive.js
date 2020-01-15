cff3c242be04f2aed1bd0b8f2d0536da68a59450
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-15 11:31:25 -0800

    Update isPositive.js

--- exercises/numbers/isPositive/isPositive.js
+++ exercises/numbers/isPositive/isPositive.js
@@ -5,16 +5,16 @@
  * @returns {boolean} True if num is positive and false otherwise
  */
 function isPositive(num) {
-  return _____;
-}
-
-if (require.main === module) {
-  console.log('Running sanity checks for isPositive:');
-
-  console.log(isPositive(0) === false);
-  console.log(isPositive(1) === true);
-  console.log(isPositive(-1) === false);
-  console.log(isPositive(-100.20) === false);
-}
-
-module.exports = isPositive;
+    return num > 0;
+  }
+  
+  if (require.main === module) {
+    console.log('Running sanity checks for isPositive:');
+  
+    console.log(isPositive(0) === false);
+    console.log(isPositive(1) === true);
+    console.log(isPositive(-1) === false);
+    console.log(isPositive(-100.20) === false);
+  }
+  
+  module.exports = isPositive;
