9a4139357cef61fc1597ea2d88bb52e7cdc3fe28
samooreSF <59896106+samooreSF@users.noreply.github.com>
2020-01-16 14:51:49 -0800

    Update remainderOf.js

--- exercises/numbers/remainderOf/remainderOf.js
+++ exercises/numbers/remainderOf/remainderOf.js
@@ -16,12 +16,55 @@
  * @returns {boolean} True if num is even and false otherwise
  */
 function remainderOf(num) {
-  /*
-    This is your job. :)
+  /**
+ * Given two positive, `n` and `d`, returns the remainder of `n` after
+ * dividing by `d`.
+ *
+ * Do not use the built-in modulo operator (`%`). The goal is to gain
+ * insight into how the modulo operator works.
+ *
+ * @example
+ * remainderOf(10, 1); // => 0
+ * remainderOf(10, 2); // => 0
+ * remainderOf(10, 3); // => 1
+ * remainderOf(10, 4); // => 2
+ * remainderOf(129, 17); // => 10
+ *
+ * @param {number} num The input number
+ * @returns {boolean} True if num is completely divisible by divisor and false otherwise
+ */
+function remainderOf(num,div) {
 
-    If you're not sure, step out of the code and use pen + paper. Start
-    with simple examples and pay attention to the process you carry out.
-  */
+  let number = num/div;
+  //console.log(number);
+  let whole_num = parseInt(number);
+  let remainder = whole_num*div;
+  let final = num-remainder;
+   if(final===0){//check if completely divisible 
+     return true;
+   }
+   else{
+     return false;
+   }
+
+  
+
+    
+  
+
+}
+
+if (require.main === module) {
+  console.log('Running sanity checks for remainderOf');
+  console.log(remainderOf(10,2));
+  console.log(remainderOf(134,15));
+
+  // Add your own sanity checks here.
+  // How else will you be sure your code does what you think it does?
+}
+
+module.exports = remainderOf;
+
 }
 
 if (require.main === module) {
