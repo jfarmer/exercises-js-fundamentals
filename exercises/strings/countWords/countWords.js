be7146b592713cdaa5d599bf911aa07c7242f891
chloedb <59859078+chloedb@users.noreply.github.com>
2020-01-16 10:47:07 -0800

    Update countWords.js

--- exercises/strings/countWords/countWords.js
+++ exercises/strings/countWords/countWords.js
@@ -9,11 +9,21 @@
 
 function countWords(string) {
   // This is your job. :)
-}
+  count = 0
+   string = string.split(' ')
 
+   for (i in string) {
+     count += 1
+   }
+   return count
+  ///count = 0 //the count of words
+}
 if (require.main === module) {
   console.log('Running sanity checks for countWords:');
-
+console.log(countWords("hi there"))
+console.log(countWords("My name is Chloe"))
+console.log(countWords(' ')) 
+//@jfarmer how do I ensure something works when it is a blank string?
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
