bc15dc21abfe9aba81c5e6165ee0d94ef4f2bb8f
jaelynztaylor <59896172+jaelynztaylor@users.noreply.github.com>
2020-01-16 14:56:34 -0800

    Update isOdd.js

--- exercises/numbers/isOdd/isOdd.js
+++ exercises/numbers/isOdd/isOdd.js
@@ -15,7 +15,7 @@
  */
 function isOdd(num) {
   // Hint: See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Remainder
-  return _____;
+  return (num % 2 === 1) ;
 }
 
 if (require.main === module) {
