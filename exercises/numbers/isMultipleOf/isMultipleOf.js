a64366c0fa0561f9208b1097bbe32d284a8d5a21
jaelynztaylor <59896172+jaelynztaylor@users.noreply.github.com>
2020-01-16 14:57:42 -0800

    Update isMultipleOf.js

--- exercises/numbers/isMultipleOf/isMultipleOf.js
+++ exercises/numbers/isMultipleOf/isMultipleOf.js
@@ -13,7 +13,7 @@
  * @returns {boolean} True if n is a multiple of d and false otherwise
  */
 function isMultipleOf(n, d) {
-  return _____;
+  return n % d === 0;
 }
 
 if (require.main === module) {
