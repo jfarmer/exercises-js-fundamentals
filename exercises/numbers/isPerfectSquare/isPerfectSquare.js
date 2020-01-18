9ac75610e5f4a37d830c96fa0ce6b8848c51ea65
Jesse Farmer <jesse@20bits.com>
2020-01-18 14:33:44 -0800

    Add isPerfectSquare

---
+++ exercises/numbers/isPerfectSquare/isPerfectSquare.js
@@ -0,0 +1,37 @@
+/**
+ * Determine whether the given integer is a perfect square.
+ *
+ * An integer `n` is a perfect square if there is some other integer `k`
+ * such that `k * k === n`. That is, if the square root of `n` is also an
+ * integer.
+ *
+ * Warning: If you use Math.sqrt, you will get incorrect answers. Try to
+ * find another way.
+ *
+ * @example
+ * isPerfectSquare(-1); // => false (no integer squared can be negative)
+ * isPerfectSquare(0); // => true
+ * isPerfectSquare(1); // => true
+ * isPerfectSquare(2); // => false
+ * isPerfectSquare(9); // => true (since 3*3 is 9)
+ *
+ * @param {number} num - An integer
+ * @returns {boolean} True if `num` is a perfect square and false otherwise.
+ */
+
+function isPerfectSauare(num) {
+  // A negative number can't be a perfect square, so immediately return false.
+  if (num < -1) {
+    return false;
+  }
+
+  // The rest is your job. :)
+}
+
+if (require.main === module) {
+  console.log('Running sanity checks for isPerfectSquare:');
+
+  // Your sanity checks go here
+}
+
+module.exports = isPerfectSauare;
