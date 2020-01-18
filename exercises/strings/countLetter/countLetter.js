661d521a7d3fa9e7b10ccc3c1a8ceccd7cda6ce7
Jesse Farmer <jesse@20bits.com>
2020-01-18 14:24:46 -0800

    Note on countLetter

--- exercises/strings/countLetter/countLetter.js
+++ exercises/strings/countLetter/countLetter.js
@@ -2,6 +2,20 @@
  * Given an input string and a target letter, returns the number of times
  * the target letter appears in the string.
  *
+ * Note:
+ * 1. You can use a `for...of` loop to iterate through the characters
+ *    of a string one letter at a time.
+ * 2. You can access a specific character using string[i], as
+ *    if the string were an array.
+ *
+ * Both of these are enough to complete this exercise.
+ *
+ * @example
+ * countLetter('hello', 'l'); // => 2
+ * countLetter('Mississippi', 's'); // => 4
+ * countLetter('Mississippi', 'x'); // => 0
+ * countLetter('Mississippi', 'm'); // => 0 (no lower-case m's)
+ *
  * @param {string} string - The string to search
  * @param {string} letter - The target letter
  * @returns {string} The number of times target letter appears in input string
