19398a01f55caed694a08a813da4de5c8fc898a9
samooreSF <59896106+samooreSF@users.noreply.github.com>
2020-01-18 15:11:38 -0800

    Update fahrenheitToCelsius.js

--- exercises/numbers/fahrenheitToCelsius/fahrenheitToCelsius.js
+++ exercises/numbers/fahrenheitToCelsius/fahrenheitToCelsius.js
@@ -6,10 +6,13 @@
  */
 function fahrenheitToCelsius(temp) {
   // This is your job. :).
+
+  return (temp-32)/1.8;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for fahrenheitToCelsius:');
+  console.log(fahrenheitToCelsius(122));
 
   // Your sanity checks go here.
 }
