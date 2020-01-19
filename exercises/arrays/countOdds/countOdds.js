bd08d24f5dd196b5de9b3db3d00241617c456c1e
chloedb <59859078+chloedb@users.noreply.github.com>
2020-01-18 20:53:07 -0800

    Update countOdds.js

--- exercises/arrays/countOdds/countOdds.js
+++ exercises/arrays/countOdds/countOdds.js
@@ -12,11 +12,24 @@
  * @returns {number} The count of even integers in the array
  */
 function countOdds(array) {
-  // This is your job. :)
+  let odds = 0;
+  //when I tried not to define this by saying "let odds;", it didn't work
+  //how do I know when i NEED to define and when not?
+
+  for (let i of array) {
+    if (i % 2 !== 0) {
+      odds += 1;
+    }
+  }
+  return odds
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for countOdds:');
 }
+ console.log(countOdds([1, 2, 3, 4, 5, 19])); // => 4
+ console.log(countOdds([10, 10, 10])); // => 0
+ console.log(countOdds([1, 1, 1, 2])); // => 3
 
 module.exports = countOdds;
+
