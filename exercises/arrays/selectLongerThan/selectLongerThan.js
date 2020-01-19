1badb511c839f28269448f79a58136e0e7728d7f
chloedb <59859078+chloedb@users.noreply.github.com>
2020-01-19 13:08:33 -0800

    Update selectLongerThan.js

--- exercises/arrays/selectLongerThan/selectLongerThan.js
+++ exercises/arrays/selectLongerThan/selectLongerThan.js
@@ -19,14 +19,25 @@
  *  strictly greater the given threshold
  */
 function selectLongerThan(array, threshold) {
-  // This is your job. :)
+  let finalArray = [];
+
+  for (string of array) {
+    if (string.length > threshold) {
+      finalArray.push(string)
+    }
+  }
+  return finalArray
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for selectLongerThan:');
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], -1) + "=== ['', 'aaa', 'bb', 'c', 'dddd']");
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 0) + "=== ['aaa', 'bb', 'c', 'dddd']");
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 1) + "=== ['aaa', 'bb', 'dddd']");
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 2) + "=== ['aaa', 'dddd']");
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 3) + "=== ['dddd']");
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 4) + "=== []");
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
 }
 
 module.exports = selectLongerThan;
