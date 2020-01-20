906035266b458733d8ca6d32741a153a75c3b03f
Patrick Sullivan <pasullivan@davidson.edu>
2020-01-19 16:56:21 -0800

    Here is my C->F exercise

--- exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
+++ exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
@@ -7,14 +7,17 @@
 
 
 function celsiusToFahrenheit(temp) {
-  let far = temp * 1.8 + 32
-  console.log(far);
+  let far;
+  far = (temp * 1.8 + 32);
+  return far;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for celsiusToFahrenheit:');
-    if(far < -459.67) { console.log('Temperature too low!');}
-    else {console.log('Temperature is: ');}
+  console.log(celsiusToFahrenheit(27) === 80.6);
+  console.log(celsiusToFahrenheit(17) === 62.6);
+  console.log(celsiusToFahrenheit(-2) === 28.4);
+  console.log(celsiusToFahrenheit(0) === 32);
 }
 
-module.exports = celsiusToFahrenheit;
+module.exports = celsiusToFahrenheit; 
