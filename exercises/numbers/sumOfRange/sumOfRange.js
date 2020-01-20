16d0d039af92d3fcc4158fedf7d1662d7f8a71a4
Patrick Sullivan <pasullivan@davidson.edu>
2020-01-19 16:58:41 -0800

    Sum Of Range

--- exercises/numbers/sumOfRange/sumOfRange.js
+++ exercises/numbers/sumOfRange/sumOfRange.js
@@ -11,14 +11,25 @@
  * @param {number} rightSummand - The right integer of the range of sum
  * @returns {boolean} The sum of every integer between the two input integers, inclusive
  */
+
 function sumOfRange(leftSummand, rightSummand) {
-  // This is your job. :)
+  const range = [];
+
+  for(let i = leftSummand; i <= rightSummand; i++)
+  {range.push(i);}
+
+let sum = range.reduce(function (accumulator, currentValue)
+{return accumulator + currentValue; })
+return sum;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for sumOfRange:');
-
-  // Your sanity checks go here
+  console.log(sumOfRange(1, 5) === 15);
+  console.log(sumOfRange(1, 6) === 21);
+  console.log(sumOfRange(-3, 4) === 4);
+  console.log(sumOfRange(-4,-2) === -9);
+  
 }
 
 module.exports = sumOfRange;
