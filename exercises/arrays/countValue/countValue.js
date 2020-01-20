7b24f5920851f705a79f561816b885b0b89d1911
samooreSF <59896106+samooreSF@users.noreply.github.com>
2020-01-19 23:16:30 -0800

    Update countValue.js

--- exercises/arrays/countValue/countValue.js
+++ exercises/arrays/countValue/countValue.js
@@ -15,11 +15,26 @@
  * @returns {number} The number of times the value appears in the array
  */
 function countValue(array, value) {
+  let count=0;
+  for(let i of array){
+
+    if(i===value){
+      count++
+
+    }
+  }
+  return count;
   // This is your job. :)
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for countValue:');
+  console.log(countValue([1, 2, 3, 4, 5], 2)); // => 1
+  console.log(countValue([1, 2, 3, 4, 5], 17)); // => 0
+  console.log(countValue([1, 2, 1, 2, 3, 4, 1, 2, 1], 1)); // => 4
+  console.log(countValue([10, 10, 10, -10], 10)); // => 3
+  console.log(countValue(['hello', 'bananas', 'hello',],'hello') == 2);
+  console.log(countValue(['hello', 'bananas', 'hello'], 'giraffe')==0);
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
