d4588bb3cc9e28b9eb7105d167e1ac151251b5ce
chloedb <59859078+chloedb@users.noreply.github.com>
2020-01-22 20:21:24 -0800

    Update selectGreaterThan.js

--- exercises/arrays/selectGreaterThan/selectGreaterThan.js
+++ exercises/arrays/selectGreaterThan/selectGreaterThan.js
@@ -4,27 +4,32 @@
  *
  * Return an empty array if no such numbers exist.
  *
- * @example
- * selectGreaterThan([1, 2, 3, 4, 5], 2); // => [3, 4, 5]
- * selectGreaterThan([1, 2, 3, 4, 5], 17); // => []
- * selectGreaterThan([1, 2, 1, 2, 3, 4, 1, 2, 1], 1); // => [2, 2, 3, 4, 2];
- * selectGreaterThan([10, 10, 10, -10, 15], 10); // => [15]
- * selectGreaterThan([10, 20, 30, 40], 100); // => []
- *
  * @param {number[]} array - An array of numbers
  * @param {number} threshold - The threshold for selecting
  * @returns {number[]} A new array consisting of all the numbers in input array
  *  strictly greater than the threshold
  */
 function selectGreaterThan(array, threshold) {
-  // This is your job. :)
+  let finalArray = []
+
+  for (number of array) {
+    if (number > threshold) {
+      finalArray.push(number)
+    }
+  }
+  return finalArray
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for selectGreaterThan:');
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log(selectGreaterThan([1, 2, 3, 4, 5], 2)); // => [3, 4, 5]
+  console.log(selectGreaterThan([1, 2, 3, 4, 5], 17)); // => []
+  console.log(selectGreaterThan([1, 2, 1, 2, 3, 4, 1, 2, 1], 1)); // => [2, 2, 3, 4, 2];
+  console.log(selectGreaterThan([10, 10, 10, -10, 15], 10)); // => [15]
+  console.log(selectGreaterThan([10, 20, 30, 40], 100)); // => []
+
 }
 
 module.exports = selectGreaterThan;
+
