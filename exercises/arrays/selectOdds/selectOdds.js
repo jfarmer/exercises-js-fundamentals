e061b2be6917c6ce773d56017b1efea726fd0cc4
chloedb <59859078+chloedb@users.noreply.github.com>
2020-01-22 20:32:19 -0800

    Update selectOdds.js

--- exercises/arrays/selectOdds/selectOdds.js
+++ exercises/arrays/selectOdds/selectOdds.js
@@ -4,24 +4,28 @@
  *
  * Return an empty array if no such numbers exist.
  *
- * @example
- * selectOdds([1, 2, 3, 4, 5]); // => [1, 3, 5]
- * selectOdds([10, 2, 19, 10, -103]); // => [19, -103]
- * selectOdds([1, 1, 2, 1]); // => [1, 1, 1]
- * selectOdds([10, 20, 30]); // => []
- *
  * @param {number[]} array - An array of numbers
  * @returns {number[]} A new array consisting of all the odd numbers in the input array
  */
 function selectOdds(array) {
-  // This is your job. :)
+  let finalArray = []
+  
+  for (let number of array) {
+    if (number % 2 !== 0) {
+      finalArray.push(number)
+    }
+  }
+  return finalArray
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for selectOdds:');
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log(selectOdds([1, 2, 3, 4, 5])); // => [1, 3, 5]
+  console.log(selectOdds([10, 2, 19, 10, -103])); // => [19, -103]
+  console.log(selectOdds([1, 1, 2, 1])); // => [1, 1, 1]
+  console.log(selectOdds([10, 20, 30])); // => []
 }
 
 module.exports = selectOdds;
+
