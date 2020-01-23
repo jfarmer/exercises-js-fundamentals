9e7ca5ceca1d856b22aed7ab0abef9efdf31ebd6
chloedb <59859078+chloedb@users.noreply.github.com>
2020-01-22 20:23:58 -0800

    Update selectLessThan.js

--- exercises/arrays/selectLessThan/selectLessThan.js
+++ exercises/arrays/selectLessThan/selectLessThan.js
@@ -4,20 +4,20 @@
  *
  * Return an empty array if no such numbers exist.
  *
- * @example
- * selectLessThan([1, 2, 3, 4, 5], 2); // => [1]
- * selectLessThan([1, 2, 3, 4, 5], 17); // => [1, 2, 3, 4, 5]
- * selectLessThan([-20, 2, 1, 0, 1, 2, 1, 2], 1); // => [-20, 0];
- * selectLessThan([10, 4, 10, -10, 15], 10); // => [4, -10]
- * selectLessThan([10, 20, 30, 40], -100); // => []
- *
  * @param {number[]} array - An array of numbers
  * @param {number} threshold - The threshold for selecting
  * @returns {number[]} A new array consisting of all the numbers in input array
  *  strictly less than the threshold
  */
 function selectLessThan(array, threshold) {
-  // This is your job. :)
+  let finalArray = []
+
+  for (number of array) {
+    if (number < threshold) {
+      finalArray.push(number)
+    }
+  }
+  return finalArray
 }
 
 if (require.main === module) {
