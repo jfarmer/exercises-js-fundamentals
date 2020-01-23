4579f00264cc20d05eef8e49289c17eed540c643
chloedb <59859078+chloedb@users.noreply.github.com>
2020-01-22 20:08:08 -0800

    Update removeEmpty.js

--- exercises/arrays/removeEmpty/removeEmpty.js
+++ exercises/arrays/removeEmpty/removeEmpty.js
@@ -2,24 +2,29 @@
  * Given an array of strings, return a new array with all the elements
  * of the original array except null, undefined, and the empty string ''.
  *
- * @example
- * removeEmpty([null, '', '']); // => []
- * removeEmpty(['Jesse', undefined, 'Farmer']); // => ['Jesse', 'Farmer']
- * removeEmpty(['one', '', 'two', '', 'three']); // => ['one', 'two', 'three']
  *
  * @param {string[]} array - An array of strings
  * @returns {string[]} A new array consisting of all the non-empty values in the
  *   input array.
  */
 function removeEmpty(array) {
-  // This is your job. :)
+  let finalArray = []
+  
+  for (char of array) {
+    if (char !== undefined && char !== '' && char !== null) {
+      finalArray.push(char)
+    }
+  }
+  return finalArray
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for removeEmpty:');
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log(removeEmpty([null, '', ''])); // => []
+  console.log(removeEmpty(['Jesse', undefined, 'Farmer'])); // => ['Jesse', 'Farmer']
+  console.log(removeEmpty(['one', '', 'two', '', 'three'])); // => ['one', 'two', 'three']
 }
 
 module.exports = removeEmpty;
+
