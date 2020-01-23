2c56381bd84ba385117c99cc20bc479d7884ce34
chloedb <59859078+chloedb@users.noreply.github.com>
2020-01-22 20:44:02 -0800

    Update sum.js

--- exercises/arrays/sum/sum.js
+++ exercises/arrays/sum/sum.js
@@ -1,22 +1,19 @@
-/**
- * Given an array of numbers, return their sum.
- *
- * @example
- * sum([1, 2, 3]); // => 6 (i.e., 1 + 2 + 3)
- * sum([-10, 10]); // => 0 (i.e., -10 + 10)
- *
- * @param {number[]} array - An array of numbers
- * @returns {number} The sum of the numbers in the array
- */
+
 function sum(array) {
-  // This is your job. :)
+  let totalSum = 0
+  
+  for (number of array) {
+    totalSum += number
+  }
+  return totalSum
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for sum:');
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log(sum([1, 2, 3])); // => 6 (i.e., 1 + 2 + 3)
+  console.log(sum([-10, 10])); // => 0 (i.e., -10 + 10)
 }
 
 module.exports = sum;
+
