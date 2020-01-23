190db9f372cc46e3fe57a04d66dd6dbd69dbd289
chloedb <59859078+chloedb@users.noreply.github.com>
2020-01-22 20:13:55 -0800

    Update selectAboveMean.js

--- exercises/arrays/selectAboveMean/selectAboveMean.js
+++ exercises/arrays/selectAboveMean/selectAboveMean.js
@@ -4,25 +4,35 @@
  *
  * Return an empty array if no such numbers exist.
  *
- * @example
- * selectAboveMean([1, 1, 1]); // => []
- * selectAboveMean([-10, -10, 0, 10]); // => [0, 10]
- * selectAboveMean([1, 2, 3, 4, 5]); // => [4, 5]
- * selectAboveMean([100, 0, 100, 200, 100, 0]); // => [100, 100, 200, 100]
- *
  * @param {number[]} array - An array of numbers
  * @returns {number[]} A new array consisting of all numbers in the input array
  *   greater than the mean.
  */
 function selectAboveMean(array) {
-  // This is your job. :)
+  let finalArray = [];
+  let meanCount = 0;
+  let meanNumber = 0;
+  
+  for (number of array) {
+    meanCount += number
+  }
+  meanNumber = meanCount / array.length
+  
+  for (number of array) {
+    if (number > meanNumber) {
+      finalArray.push(number)
+    }
+  }
+  return finalArray
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for selectAboveMean:');
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log(selectAboveMean([1, 1, 1])); // => []
+  console.log(selectAboveMean([-10, -10, 0, 10])); // => [0, 10]
+  console.log(selectAboveMean([1, 2, 3, 4, 5])); // => [4, 5]
+  console.log(selectAboveMean([100, 0, 100, 200, 100, 0])); // => [100, 100, 200, 100]
 }
 
 module.exports = selectAboveMean;
