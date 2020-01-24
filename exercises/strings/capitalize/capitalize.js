369641d42f7daa7052ab65562f788f25b2e20330
chloedb <59859078+chloedb@users.noreply.github.com>
2020-01-23 22:01:49 -0800

    Update capitalize.js

--- exercises/strings/capitalize/capitalize.js
+++ exercises/strings/capitalize/capitalize.js
@@ -11,14 +11,30 @@
  */
 
 function capitalize(num) {
-  // This is your job. :)
+  // take the string
+  //find the ascii codes
+  //find where they need to be + what number??
+  let finalString = '';
+  let newAscii = 0;
+  
+  for (let element of num) {
+    if (element.charCodeAt(num) >= 97 && element.charCodeAt(num) <= 122) {
+      newAscii = Number(element.charCodeAt(num)) - 32;
+      finalString = finalString + String.fromCharCode(newAscii);
+    }
+    else {
+      finalString = finalString + element
+    }
+  }
+  return finalString
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for capitalize:');
-
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log(capitalize('I am listening to SZA') === 'I AM LISTENING TO SZA')
+  console.log(capitalize('WORDS') === 'WORDS')
+  console.log(capitalize('lowercase string') === 'LOWERCASE STRING')
 }
 
 module.exports = capitalize;
+
