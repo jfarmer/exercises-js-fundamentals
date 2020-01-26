30a026db7947a6aa04a420418218c730606fbde3
Chloe deBeus <chdebeus@davidson.edu>
2020-01-26 15:51:21 -0800

    here is a third piece of code for this branch

--- exercises/strings/toSnakeCase/toSnakeCase.js
+++ exercises/strings/toSnakeCase/toSnakeCase.js
@@ -14,12 +14,29 @@
  */
 
 function toSnakeCase(string) {
-  // This is your job. :)
+  let finalString = ''
+  
+  for (character of string) {  
+    let newAscii = character.charCodeAt(string)
+    //console.log('here we are: ', ' '.charCodeAt())
+      if (character.charCodeAt(string) >= 65 && character.charCodeAt(string) <= 90) {
+        newAscii = Number(character.charCodeAt(string)) + 32
+      finalString =finalString + String.fromCharCode(newAscii)
+      }
+      if (character.charCodeAt(string) === 32) {
+        finalString = finalString + String.fromCharCode(95)
+      }
+      else {
+      finalString =finalString + character
+    }
+  }
+  //console.log(finalString)
+  return finalString
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for toSnakeCase:');
-
+  console.log(toSnakeCase('hi there we are spaces') === 'hi_there_we_are_spaces')
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
