f0c8a624ad1d6814cd1e8a6b068a2364b2f5c75c
Patrick Sullivan <pasullivan@davidson.edu>
2020-01-28 14:31:14 -0800

    Here is my solid pyramid exercise, Jesse

--- exercises/patterns/printSolidPyramid/printSolidPyramid.js
+++ exercises/patterns/printSolidPyramid/printSolidPyramid.js
@@ -25,42 +25,47 @@ let helpers = require('../printHelpers');
  *  #######
  * #########
  *
- * @param {number} height - The height of the pyramid to print
+ * @param {number} h - The height of the pyramid to print
  */
-function printSolidPyramid(height) {
-  /*
+function printSolidPyramid(h) {
+  let numChars = 1;
+  let numSpaces = h-1;
+   /*
     Reflect: given `height`...
     1. How many lines to we want to print?
     2. How many spaces are start a given line, followed by how many # characters?
-
     Work through 3-4 examples by hand and try to notice a pattern.
   */
-  for (let i = 0; i < height; i++) {
-    let numSpaces = _____;
-    let numChars = _____;
+ for (let i = 0; i < h; i++) {
+  //console.log(numSpace);
+  
+  //console.log(numChars);
 
-    helpers.printCountTimes(' ', numSpaces);
-    helpers.printCountTimes('#', numChars);
-    helpers.printNewLine();
-  }
+  helpers.printCountTimes(' ', numSpaces);
+  helpers.printCountTimes('#', numChars);
+  helpers.printNewLine();
+
+  numChars = numChars + 2;
+  numSpaces = numSpaces - 1;
+}
 }
 
 /**
- * For testing purposes, prints a diagram of the given height.
- */
+* For testing purposes, prints a diagram of the given height.
+*/
 function solidPyramidPrintTest(height) {
-  console.log('');
-  console.log(`Printing a SOLID PYRAMID of height ${height}:`);
+console.log('');
+console.log(`Printing a SOLID PYRAMID of height ${height}:`);
 
-  printSolidPyramid(height);
+printSolidPyramid(height);
 }
 
 if (require.main === module) {
-  solidPyramidPrintTest(1);
-  solidPyramidPrintTest(2);
-  solidPyramidPrintTest(3);
-  solidPyramidPrintTest(6);
-  solidPyramidPrintTest(10);
+solidPyramidPrintTest(1);
+solidPyramidPrintTest(2);
+solidPyramidPrintTest(3);
+solidPyramidPrintTest(6);
+solidPyramidPrintTest(10);
 }
 
 module.exports = printSolidPyramid;
