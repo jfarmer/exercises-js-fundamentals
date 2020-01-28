373a42a0b3f4c88286b104981b3903b5c82194f2
jaelynztaylor <jaelynztaylor@users.noreply.github.com>
2020-01-28 13:57:31 -0800

    Final code for printSolidRightTriangle.js

--- exercises/patterns/printSolidRightTriangle/printSolidRightTriangle.js
+++ exercises/patterns/printSolidRightTriangle/printSolidRightTriangle.js
@@ -32,11 +32,11 @@ function printSolidRightTriangle(height) {
     1. How many lines to we want to print?
     2. How many characters should be on each line?
   */
-  for (let i = 0; i < height; i++) {
-    let numChars = _____;
+ for (let i = 0; i < height; i++) {
+  let numChars = i + 1
 
-    helpers.printCountTimes('#', numChars);
-    helpers.printNewLine();
+  helpers.printCountTimes('#', numChars);
+  helpers.printNewLine();
   }
 }
 
