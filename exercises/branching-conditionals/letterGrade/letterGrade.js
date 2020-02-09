aa32a95f370f0844ff456fbe04c8be07e30f3b37
Arian <razzaghi2012@gmail.com>
2020-02-08 22:10:04 -0600

    fixing the letterGrade... took me about 1hr and  15 min

--- exercises/branching-conditionals/letterGrade/letterGrade.js
+++ exercises/branching-conditionals/letterGrade/letterGrade.js
@@ -13,12 +13,72 @@
  *  a student's percentage grade.
  * @return {string} The corresponding letter grade for the given percentage grade
  */
+
+/**
+ * 
+ * @ARIANS devised plan 
+ * 1/ ensure that the number we put into the function is actually an integer
+ * 2/ create some condition that for anything below 60 equals F
+ * 3/ create conditions for "-" which is a minus grade, for non-plus or non-minus grade, and for "+"" grades
+ * 4/ i'm not sure if i should create multiple else ifs or if i should  create an array and access the key  values
+ * 5/ return  the value of the percentGrade with a value "letter" and a minus/plus cuttoff
+
+ */
+
 function letterGrade(percentGrade) {
+  let gradePluses = [67,68,69,77,78,79,87,88,89,97,98,99];
+  let gradeMinus = [60,61,62,70,71,72,80,81,82,90,91,92];
+  let gradeNormal = [63,64,65,73,74,75,83,84,85,93,94,95];  
+  
+  if (percentGrade < 60 ) {
+    return "F" ;
+  }
+  else if ( percentGrade >= 60 && percentGrade <= 62) {
+  return "D-";
+  } 
+    else if ( percentGrade >= 63 && percentGrade <= 66) {
+      return "D";
+      }
+      else if ( percentGrade >= 67 && percentGrade <= 69) {
+        return "D+";
+        }
+
+  else if ( percentGrade >= 70 && percentGrade <= 72) {
+  return "C-";
+  } 
+    else if ( percentGrade >= 73 && percentGrade <= 76) {
+      return "C";
+      }
+      else if ( percentGrade >= 77 && percentGrade <= 79) {
+        return "C+";
+        }
+   else if ( percentGrade >= 80 && percentGrade <= 82) {
+  return "B-";
+  } 
+    else if ( percentGrade >= 83 && percentGrade <= 86) {
+      return "B";
+      }
+      else if ( percentGrade >= 87 && percentGrade <= 89) {
+        return "B+";
+        }
+  else if ( percentGrade >= 90 && percentGrade <= 92) {
+  return "A-";
+  } 
+    else if ( percentGrade >= 93 && percentGrade <= 96) {
+      return "A";
+      }
+      else if ( percentGrade >= 97 && percentGrade <= 99) {
+        return "A+";
+        }
+
   // This is your job. :)
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for letterGrade:');
+  console.log(letterGrade(55));
+  console.log(letterGrade(69));
+  console.log(letterGrade(99));
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
