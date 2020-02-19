b965eaecad6ed59983e63b1ecbf67431dc374e24
Arian <razzaghi2012@gmail.com>
2020-02-19 12:44:03 -0800

    sbmitting and not sure why the answer isn't working. spent 1hrt on this and im pissed and need some help after not coding for 5 days.

--- exercises/arrays/sum/sum.js
+++ exercises/arrays/sum/sum.js
@@ -8,12 +8,28 @@
  * @param {number[]} array - An array of numbers
  * @returns {number} The sum of the numbers in the array
  */
+
+/**
+ * arian's thoughts
+ * 1. return a value for a number in an array
+ * 2. ensure that you can add 2 numbers consistenly
+ * 3. ensure that you return the correct final answer through sanity checks
+ * 
+ * 
+ */
+
 function sum(array) {
-  // This is your job. :)
+  let sum = 0;
+  for (let i = 0; i < array.length; i ++) {
+    let totalSum = sum + array[i]; 
+    console.log (totalSum);  
+} 
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for sum:');
+  console.log(sum([1,2,3]));
+  console.log(sum([1,2,3])=== 6);
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
