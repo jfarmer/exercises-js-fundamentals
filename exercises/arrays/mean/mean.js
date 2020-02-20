2c1aeb189a9df934595d776a5118874ed53c20b8
Arian <razzaghi2012@gmail.com>
2020-02-19 17:53:05 -0800

    mean exercise... still not getting the loop value

--- exercises/arrays/mean/mean.js
+++ exercises/arrays/mean/mean.js
@@ -19,11 +19,36 @@
  * @returns {number} The mean of the numbers in the array
  */
 function mean(array) {
-  // This is your job. :)
+  console.log (array.length);
+  let count = array[0];
+  for (i = 0; i < array.length ; i++) {
+    let numbers = array[i];
+    let meanNumbers= ( (numbers + count ) / (array.length));
+    console.log ("this is the answer to" + " " + meanNumbers);
+    ;
+  }
 }
+
+/**
+ * this is the output in my terminal 
+ * Running sanity checks for mean:
+3
+this is the answer to 6.666666666666667
+this is the answer to 10
+this is the answer to 13.333333333333334
+false
+
+@ARIAN so i see the divison is working
+the loop seems to working 
+the addition is not. i feel like something is happening where i know i should take a value and save it, then iterate to the next value and add it to the saved value and then add the third... but this is where im getting stuck.
+ * 
+ */
+
+
 
 if (require.main === module) {
   console.log('Running sanity checks for mean:');
+  console.log (mean([10,20,30]) == 20) ;
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
