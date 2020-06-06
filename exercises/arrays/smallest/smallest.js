6d50e17dd8a01a6376cb5290f6337f303f2f7a2f
Jesse Farmer <jesse@20bits.com>
2020-06-06 16:45:20 -0700

    Update ESLint + config

--- exercises/arrays/smallest/smallest.js
+++ exercises/arrays/smallest/smallest.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-self-assign */
+/* eslint-disable no-use-before-define */
+
 /**
  * Given an array, returns the smallest element in the array.
  *
@@ -7,6 +10,12 @@
  * @returns {number} The smallest element in the array
  */
 function smallest(array) {
+  // The _____ on each of the lines below are meant to act
+  // like a fill-in-the-blank so you can see the structure.
+  //
+  // Replace each _____ with some code (not all blanks) will
+  // use the same code.
+
   let _____ = _____;
 
   for (let _____ of _____) {
