47ce0bf85b5f5d195629f68272bec59b6ffa0f05
Akua Owusu <akowusu@davidson.edu>
2020-06-15 19:20:37 -0400

    I think I did it

--- exercises/strings/capitalize/capitalize.js
+++ exercises/strings/capitalize/capitalize.js
@@ -10,13 +10,19 @@
  * @returns {string} A capitalized copy of the string
  */
 
-function capitalize(num) {
-  // This is your job. :)
+function capitalize(string) {
+  string1 = string.slice(0,1);
+  string2 = string1.toUpperCase();
+  string3 = string.slice(1);
+  string4 = string2 + string3;
+  return string4
 }
 
 if (require.main === module) {
-  console.log('Running sanity checks for capitalize:');
-
+  console.log(capitalize('Running sanity checks for capitalize:'));
+  console.log(capitalize('on my way'));
+  console.log(capitalize('running now'));
+  console.log(capitalize('string'));
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
